44f9c5c168b638d92969694a8fa628600f7419b5
StenQuidelleur <sten.quidelleur@outlook.fr>
2020-07-22 18:44:01 +0200

    create the hhome page with other page of navigation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use App\Repository\BannerRepository;
+use App\Repository\CategoryPerfRepository;
+use App\Repository\PerformanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +15,48 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home_index")
+     * @param BannerRepository $banner
+     * @param CategoryPerfRepository $category
      * @return Response
      */
-    public function index(): Response
+    public function index(BannerRepository $banner,CategoryPerfRepository $category): Response
     {
-        return $this->render('home/index.html.twig');
+        $banner = $banner->findAll();
+        $categories = $category->findAll();
+        return $this->render('home/index.html.twig', [
+            'banner' => $banner[0],
+            'categories' => $categories
+        ]);
     }
 
+    /**
+     * @Route("/performances", name="performances")
+     * @param PerformanceRepository $performance
+     * @return Response
+     */
+    public function performances(PerformanceRepository $performance): Response
+    {
+        $performances = $performance->findAll();
+        return $this->render('home/performances.html.twig', [
+            'performances' => $performances
+        ]);
+    }
+
+    /**
+     * @Route("/aboutus", name="aboutus")
+     * @return Response
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render('home/aboutus.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     * @return Response
+     */
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
 }
